9ef9a3fd853eb5e48fab36dfbc1ac34f53e6f772
kiet <kiet.nguyen.intern@ntq-solution.com.vn>
2019-09-26 11:13:51 +0700

    set timeout

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
 
 
   root 'home#index'
-  delete '/logout',  to: 'sessions#destroy'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 end
