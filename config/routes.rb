113b062674a5d5ea30cbb8582dff350ba5d17f2b
kiet <kiet.nguyen.intern@ntq-solution.com.vn>
2019-10-15 16:19:46 +0700

    final pending

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,18 @@ Rails.application.routes.draw do
   get "content/edit" => 'content#edit', :as => :edit
   get  '/set-captain',   to: 'roles#setCaptain',      as: "set_captain"
   get  '/set-mentor',    to: 'roles#setMentor' ,      as: "set_mentor"
+
   get  '/delete-mentor', to: 'roles#destroyMentor',   as: "del_mentor"
   get  '/delete-member', to: 'roles#destroyMember',   as: "del_member"
+
   get  '/join-group',    to: 'join_groups#joinGroup', as: "join_group"
   get  '/leave-group',    to: 'join_groups#leaveGroup', as: "leave_group"
+
+  get  '/approve-content',    to: 'pendings#approveContent', as: "approve_content"
+  get  '/decline-content',    to: 'pendings#declineContent', as: "decline_content"
+
+  get  '/approve-member',    to: 'pendings#approveMember', as: "approve_member"
+  get  '/decline-member',    to: 'pendings#declineMember', as: "decline_member"
 
   root 'home#index'
 
